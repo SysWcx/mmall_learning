7a69a2729831edc954cd0a7b1c6cb4b655a9e52f
SysWcx <286277507@qq.com>
2017-08-28 09:27:40 +0800

    first commit init project

---
+++ src/main/java/example/HelloWorld.java
@@ -0,0 +1,10 @@
+package example;/**
+ * Created by machenike on 2017/8/23.
+ */
+public class HelloWorld {
+  public String sayHelloWorldFrom(String from) {
+    String result = "Hello, world, from " + from;
+    System.out.println(result);
+    return result;
+  }
+}
